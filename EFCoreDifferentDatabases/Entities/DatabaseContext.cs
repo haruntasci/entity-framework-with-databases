959d12881ec3f5ce8bc7024598f66f2949b853b5
haruntasci <haruntasci07@gmail.com>
2023-04-11 18:40:53 +0300

    Add project files.

---
+++ EFCoreDifferentDatabases/Entities/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreDifferentDatabases.Entities
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DatabaseContext()
+        {
+            
+        }
+
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+    }
+}
